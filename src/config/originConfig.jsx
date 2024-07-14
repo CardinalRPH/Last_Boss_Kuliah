585dcdea55885bdfd392affb74aea5aeb4cf7dc9
CardinalRPH <rayhanfebriyan62@gmail.com>
2024-07-14 17:49:36 +0700

    Change Origin

--- src/config/originConfig.jsx
+++ src/config/originConfig.jsx
@@ -1,2 +1,2 @@
-export const httpURI = 'http://192.168.1.12:5000/app'
-export const wsURI = 'ws://192.168.1.12:5000'
+export const httpURI = 'http://192.168.172.237:5000/app'
+export const wsURI = 'ws://192.168.172.237:5000'
